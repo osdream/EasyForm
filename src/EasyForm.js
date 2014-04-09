88afb6b13bfcaf170986c985ae8a8913801f0aae
songao <songao@baidu.com>
2014-04-09 17:43:27 +0800

    first commit

---
+++ src/EasyForm.js
@@ -0,0 +1,47 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+ * $Id$
+ * 
+ **************************************************************************/
+ 
+ 
+/*
+ * path:    src/EasyForm.js
+ * desc:    EasyForm constructor
+ * author:  songao(songao@baidu.com)
+ * version: $Revision$
+ * date:    $Date: 2014/04/09 17:41:51$
+ */
+
+define(function(require) {
+    /**
+     * EasyForm 构造函数
+     * @constructor
+     */
+    function EasyForm() {
+        // TODO
+    }
+
+    return EasyForm;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 : */
